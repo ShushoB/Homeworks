7f70bce220b3e427371eb53dcb23c602d9066049
ShushoB <114217901+ShushoB@users.noreply.github.com>
2022-09-23 19:22:33 +0400

    Add files via upload

---
+++ Py2.py
@@ -0,0 +1,6 @@
+ch = input("Enter a Character : ")
+vowels = [ 'a', 'A', 'e', 'E', 'u', 'U', 'i', 'I', 'o', 'O']
+if ch  in vowels:
+    print(ch, "is a vowel")
+else:
+    print(ch, "is a consonant")
